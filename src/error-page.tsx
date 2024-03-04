e991475abcdb2f98185dbfa5f9eda3a40ff34648
Cecil Thomas <me@cecil-thomas.com>
2024-03-04 10:56:44 -0700

    fixed some build errors

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -8,9 +8,6 @@ export default function ErrorPage() {
     <div className="flex flex-col justify-center text-center pt-64">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
     </div>
   );
 }
--- src/routes/wrpb.tsx
+++ src/routes/wrpb.tsx
@@ -1,4 +1,3 @@
-//import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import type { PersonalBest } from "../types/types";
 
