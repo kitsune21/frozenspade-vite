fd401fc9c14efb6bbae3c118938f595b3fa71ba2
Cecil Thomas <me@cecil-thomas.com>
2024-03-11 21:04:00 -0600

    link and better mobile view

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,21 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="absolute top-0 flex h-14 w-full bg-second px-4 py-2 text-third sm:pl-2 sm:pr-5">
-      <h2 className="border-b-2 border-fourth text-4xl">
-        <Link to="/">FrozenSpade.TV</Link>
-      </h2>
-      <ul className="flex w-full items-center justify-end gap-2">
-        <li className="text-lg transition-all duration-100 ease-in hover:border-b hover:border-fourth hover:text-fourth">
-          <Link to="/wrpb">World Records & Personal Bests</Link>
+    <nav className="absolute top-0 flex flex-col z-10 sm:flex-row sm:items-center h-auto sm:h-14 w-full bg-second px-4 py-2 text-third sm:pl-2 sm:pr-5">
+      <div className="flex w-full">
+        <h2 className="border-b-2 border-fourth text-4xl">
+          <Link to="/" onClick={() => setIsOpen(false)}>
+            FrozenSpade.TV
+          </Link>
+        </h2>
+        <button
+          className="sm:hidden ml-auto block text-4xl focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? "×" : "☰"}
+        </button>
+      </div>
+      <ul
+        className={`sm:flex w-full sm:w-auto items-center justify-end gap-4 ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <li className="flex-shrink-0 text-lg transition-all duration-100 ease-in hover:border-b hover:border-fourth hover:text-fourth">
+          <Link to="/wrpb" onClick={() => setIsOpen(false)}>
+            World Records & Personal Bests
+          </Link>
         </li>
-        <li className="text-lg transition-all duration-100 ease-in hover:border-b hover:border-fourth hover:text-fourth">
-          <Link to="/youtube">YouTube Vods</Link>
+        <li className="flex-shrink-0 text-lg transition-all duration-100 ease-in hover:border-b hover:border-fourth hover:text-fourth">
+          <Link to="/youtube" onClick={() => setIsOpen(false)}>
+            YouTube Vods
+          </Link>
         </li>
-        {/* <li className="text-lg transition-all duration-100 ease-in hover:border-b hover:border-fourth hover:text-fourth">
-          <Link to="/social">Social Media</Link>
-        </li> */}
+        <li className="flex-shrink-0 text-lg transition-all duration-100 ease-in hover:border-b hover:border-fourth hover:text-fourth">
+          <Link to="/social" onClick={() => setIsOpen(false)}>
+            Socials
+          </Link>
+        </li>
       </ul>
     </nav>
   );
--- src/routes/social.tsx
+++ src/routes/social.tsx
@@ -1,7 +1,29 @@
+import { useEffect } from "react";
+
 export default function Social() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div>
-      <p>Social goes here</p>
+    <div className="flex h-full overflow-x-auto">
+      <div className="w-full sm:w-1/3 overflow-y-auto">
+        <a
+          className="twitter-timeline"
+          data-chrome="noborders"
+          data-theme="dark"
+          href="https://twitter.com/frozenspade?ref_src=twsrc%5Etfw"
+        >
+          Tweets by frozenspade
+        </a>
+      </div>
     </div>
   );
 }
